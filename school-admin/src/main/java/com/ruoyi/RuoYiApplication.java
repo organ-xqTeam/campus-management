33fdd633a451219cdb1f1a631b7f29f336a107ac
XingPanST <XingPanST@DESKTOP-E2I46F7>
2020-01-23 09:28:28 +0800

    activity 更新可用

--- school-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ school-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -1,5 +1,6 @@
 package com.ruoyi;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * 
  * @author ruoyi
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
 public class RuoYiApplication
 {
     public static void main(String[] args)
