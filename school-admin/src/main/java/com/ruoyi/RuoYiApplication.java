db6702f23d28eb3cd97acccae6c5e119b5998d2c
Administrator <Administrator@app>
2020-02-03 20:49:52 +0800

    提交测试

--- school-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ school-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -15,6 +15,7 @@ public class RuoYiApplication
 {
     public static void main(String[] args)
     {
+    
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
     }
