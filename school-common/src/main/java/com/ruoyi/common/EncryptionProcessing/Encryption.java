4c0a8bdb64d5cf3a5d7a55092e145da6058e905f
XingPanST <XingPanST@DESKTOP-E2I46F7>
2020-02-11 11:25:47 +0800

    换加密 方法

--- school-common/src/main/java/com/ruoyi/common/EncryptionProcessing/Encryption.java
+++ school-common/src/main/java/com/ruoyi/common/EncryptionProcessing/Encryption.java
@@ -1,23 +1,25 @@
 package com.ruoyi.common.EncryptionProcessing;
 
 import com.ruoyi.common.exception.BusinessException;
+import com.ruoyi.common.utils.AES.AESUtil;
 import com.ruoyi.common.utils.SM.SM4Utils;
 
 public class Encryption {
 	
 	//控制时候加密 解密处理
 	private static boolean flag = true;
-	private static String tKey ="JeF8U9wHFOMfs2Y8";
+	private static String tKey ="1234567899876543";
+	private static String tiv ="0000000000000000";
+	
+	private static String KEY = "1234567899876543";
+    private static String IV = "0000000000000000";
 	//加密
 	public static String encryptRSA(String result) throws BusinessException{
-		SM4Utils sm4 = new SM4Utils();  
-        sm4.setSecretKey(tKey);  
-        sm4.setHexString(false);  
-        System.out.println("ECB模式加密");  
+        System.out.println("aes模式加密");  
         System.out.println(result);  
         String cipherText;
 		try {
-			cipherText = sm4.encryptData_ECB(result);
+			cipherText = AESUtil.encrypt(result, tKey, tiv);
 		} catch (Exception e) {
 			throw new BusinessException("无法加密"+result);
 		}  
@@ -32,16 +34,13 @@ public class Encryption {
 	}
 //	解密
 	public static String dncryptRSA(String result)  {
-		SM4Utils sm4 = new SM4Utils();  
-		sm4.setSecretKey(tKey);  
-		sm4.setHexString(false);  
-		System.out.println("ECB模式解密");  
+		System.out.println("aes模式解密");  
 //		String cipherText = sm4.encryptData_ECB(result);  
 //		System.out.println("密文: " + cipherText);  
 		System.out.println(result);  
         String plainText;
 		try {
-			plainText = sm4.decryptData_ECB(result);
+			plainText = AESUtil.desEncrypt(result, tKey, tiv);
 		} catch (Exception e) {
 			throw new BusinessException("无法解密"+result);
 		}  
--- school-common/src/main/java/com/ruoyi/common/utils/SM/SM4Utils.java
+++ school-common/src/main/java/com/ruoyi/common/utils/SM/SM4Utils.java
@@ -133,32 +133,26 @@ public class SM4Utils
         }  
           
     
-    public static void main(String[] args) throws IOException 
-    {
-        String plainText ="{\"username\":\"usertest0561\",\"password\":\"1234qwer\",\"rememberMe\":\"true\"}";
-        SM4Utils sm4 = new SM4Utils();
-        sm4.setSecretKey("JeF8U9wHFOMfs2Y8");
-        plainText.getBytes("UTF-8");        
-        System.out.println("ECB模式");
-        String cipherText = sm4.encryptData_ECB(plainText);
-        System.out.println("密文: " + cipherText);
-        System.out.println("");
-        
-        plainText = sm4.decryptData_ECB("EsK2Nj6jsttCt58Igvj9w/BFcjz7Yl8nO2qHaOWFTRHYaxmVKDnaveB3X+0kJPxCydS2mcq4I5vu3az13j/OJpQSn5Lk80CatOvqQeynQMRxXdmk5aZv1Wb5cF8rhrnldjbtGey93CF5EiI06018Mc1K8BJTvgOqzPKoCp3aawfwRXI8+2JfJztqh2jlhU0R2GsZlSg52r3gd1/tJCT8QsnUtpnKuCOb7t2s9d4/ziaUEp+S5PNAmrTr6kHsp0DEcV3ZpOWmb9Vm+XBfK4a55XY27RnsvdwheRIiNOtNfDHNSvASU74DqszyqAqd2msH8EVyPPtiXyc7aodo5YVNEdhrGZUoOdq94Hdf7SQk/ELJ1LaZyrgjm+7drPXeP84mlBKfkuTzQJq06+pB7KdAxHFd2aTlpm/VZvlwXyuGueV2Nu0Z7L3cIXkSIjTrTXwxzUrwElO+A6rM8qgKndprB/BFcjz7Yl8nO2qHaOWFTRHYaxmVKDnaveB3X+0kJPxCydS2mcq4I5vu3az13j/OJpQSn5Lk80CatOvqQeynQMRxXdmk5aZv1Wb5cF8rhrnldjbtGey93CF5EiI06018Mc1K8BJTvgOqzPKoCp3aawfwRXI8+2JfJztqh2jlhU0R2GsZlSg52r3gd1/tJCT8QsnUtpnKuCOb7t2s9d4/ziaUEp+S5PNAmrTr6kHsp0DEcV3ZpOWmb9Vm+XBfK4a55XY27RnsvdwheRIiNOtNfDHNSvASU74DqszyqAqd2msH8EVyPPtiXyc7aodo5YVNEdhrGZUoOdq94Hdf7SQk/ELJ1LaZyrgjm+7drPXeP84mlBKfkuTzQJq06+pB7KdAxHFd2aTlpm/VZvlwXyuGueV2Nu0Z7L3cIXkSIjTrTXwxzUrwElO+A6rM8qgKndprB/BFcjz7Yl8nO2qHaOWFTRHYaxmVKDnaveB3X+0kJPxCydS2mcq4I5vu3az13j/OJpQSn5Lk80CatOvqQeynQMRxXdmk5aZv1Wb5cF8rhrnldjbtGey93CF5EiI06018Mc1K8BJTvgOqzPKoCp3aawfwRXI8+2JfJztqh2jlhU0R2GsZlSg52r3gd1/tJCT8QsnUtpnKuCOb7t2s9d4/ziaUEp+S5PNAmrTr6kHsp0DEcV3ZpOWmb9Vm+XBfK4a55XY27RnsvdwheRIiNOtNfDHNSvASU74DqszyqAqd2msH8EVyPPtiXyc7aodo5YVNEdhrGZUoOdq94Hdf7SQk/ELJ1LaZyrgjm+7drPXeP84mlBKfkuTzQJq06+pB7KdAxHFd2aTlpm/VZvlwXyuGueV2Nu0Z7L3cIXkSIjTrTXwxzUrwElO+A6rM8qgKndprB/BFcjz7Yl8nO2qHaOWFTRHYaxmVKDnaveB3X+0kJPxCydS2mcq4I5vu3az13j/OJpQSn5Lk80CatOvqQeynQMRxXdmk5aZv1Wb5cF8rhrnldjbtGey93CF5EiI06018Mc1K8BJTvgOqzPKoCp3aawfwRXI8+2JfJztqh2jlhU0R2GsZlSg52r3gd1/tJCT8QsnUtpnKuCOb7t2s9d4/ziaUEp+S5PNAmrTr6kHsp0DEcV3ZpOWmb9Vm+XBfK4a55XY27RnsvdwheRIiNOtNfDHNSvASU74DqszyqAqd2msH8EVyPPtiXyc7aodo5YVNEdhrGZUoOdq94Hdf7SQk/ELJ1LaZyrgjm+7drPXeP84mlBKfkuTzQJq06+pB7KdAxHFd2aTlpm/VZvlwXyuGueV2Nu0Z7L3cIXkSIjTrTXwxzUrwElO+A6rM8qgKndprB/BFcjz7Yl8nO2qHaOWFTRHYaxmVKDnaveB3X+0kJPxCydS2mcq4I5vu3az13j/OJpQSn5Lk80CatOvqQeynQMRxXdmk5aZv1Wb5cF8rhrnldjbtGey93CF5EiI06018Mc1K8BJTvgOqzPKoCp3aawfwRXI8+2JfJztqh2jlhU0R2GsZlSg52r3gd1/tJCT8QsnUtpnKuCOb7t2s9d4/ziaUEp+S5PNAmrTr6kHsp0DE");
-        System.out.println("明文: " + plainText);
-        System.out.println("");
-        
-//        System.out.println("CBC模式");  
-//        sm4.iv = "UISwD9fW6cFh9SNS";  
-//        cipherText = sm4.encryptData_CBC(plainText);  
-//        System.out.println("密文: " + cipherText);  
-//        System.out.println("");  
-//          
-//        plainText = sm4.decryptData_CBC(cipherText);  
-//        System.out.println("明文: " + plainText);
-        //PI4ke7HMoUMD/LOSHWX5/g==
-        
-    }
+	/*
+	 * public static void main(String[] args) throws IOException { String plainText
+	 * ="{\"username\":\"usertest0561\",\"password\":\"1234qwer\",\"rememberMe\":\"true\"}";
+	 * SM4Utils sm4 = new SM4Utils(); sm4.setSecretKey("JeF8U9wHFOMfs2Y8");
+	 * plainText.getBytes("UTF-8"); System.out.println("ECB模式"); String cipherText =
+	 * sm4.encryptData_ECB(plainText); System.out.println("密文: " + cipherText);
+	 * System.out.println("");
+	 * 
+	 * plainText = sm4.decryptData_ECB("v5oJ+QH8358MGxG0GuZoBQ==");
+	 * System.out.println("明文: " + plainText); System.out.println("");
+	 * 
+	 * System.out.println("CBC模式"); sm4.iv = "UISwD9fW6cFh9SNS"; cipherText =
+	 * sm4.encryptData_CBC(plainText); System.out.println("密文: " + cipherText);
+	 * System.out.println("");
+	 * 
+	 * plainText = sm4.decryptData_CBC(cipherText); System.out.println("明文: " +
+	 * plainText); //PI4ke7HMoUMD/LOSHWX5/g==
+	 * 
+	 * }
+	 */
 
 	public String getSecretKey() {
 		return secretKey;
